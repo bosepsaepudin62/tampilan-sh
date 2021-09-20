b1c6598fc65065d757973467b1f9f61ba9c55257
bosepsaepudin62 <90843556+bosepsaepudin62@users.noreply.github.com>
2021-09-20 09:34:24 +0700

    Add files via upload

---
+++ tampilan.sh
@@ -0,0 +1,33 @@
+#!bin/sh
+#code by MR.OLED KRAWANG
+#scrip perubahan tampilan termux
+#tampilan
+tam1=">><<>><<>><<>><<>><<>><<>>"
+tam2="{  wellcom we Are user karawang  }"                          #login termux
+toilet -f big -F gay LOGIN
+
+login termux
+toilet -f big -F gay LOGIN
+echo "masukan passwod"| lolcat
+read pass
+
+# data tampilan
+if [ $pass = oled 62 ]
+then
+      echo "masukan title"
+      read title
+      clear
+      figlet $title | lolcat
+      echo $tam1 | lolcat tampilan 1
+      echo $tam2 | lolcat tampilan 2
+      echo $tam1 | lolcat tampilan 1
+      echo
+  echo "# scurity cyber" | lolcal
+  echo "# cyber army" | lolcat
+  echo "# karawang 62 cyber BELCKHAET | lolcat
+  echo $tam 1 lolcat # tampilan 1
+   else
+echo "passwod salah" | lolcat
+echo $tam1 | lolcat # tampilan 1
+sh tampilan.sh
+  fi
